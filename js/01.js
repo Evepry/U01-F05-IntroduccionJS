ce48725f9312f6feef9ca4766573c1956d2eb6eb
Priscy Carrillo <priscy.carrillo42@gmail.com>
2024-01-23 17:53:55 -0600

    C104M20240123

---
+++ js/01.js
@@ -0,0 +1,33 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar una variable y asignarle valor
+var disponible; // Iniciamos una variable pero sin valor
+alert (producto);
+
+producto = true; // Reasignando el valor de la variable
+
+alert (producto); 
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+    alert (producto1 + disponible1 + categoria);
+
+// Variables no permitidas
+/*
+var 1disponible;
+var nombre-producto;
+*/
+
+// Variables permitidas
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'monitor HD'; // underscore
+var nombreProducto = 'monitorHD';   // Camelcase
+var NombreProducto = 'Monitor HD';  // Pascal Case
+var nombreproducto = 'monitorhd';
+
+console.log(producto);
+console.log('Hola Mundo');
