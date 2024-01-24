6750b7063a22f32069893ac796d464df247a9723
Priscy Carrillo <priscy.carrillo42@gmail.com>
2024-01-24 17:40:39 -0600

    C109M20240124

---
+++ js/06.js
@@ -0,0 +1,25 @@
+// Objeto Math
+
+// ventana global*
+/*
+Escribir window en la consola
+**probar Math.random();
+*/
+let resutado;
+
+resutado = Math.PI;
+resutado = Math.round(2.5);
+
+resutado1 = Math.ceil(2.2); //redondea hacia arriba
+resutado2 = Math.floor(2.9); //redondea hacia abajo
+console.log(resutado1);
+console.log(resutado2);
+
+resutado = Math.sqrt(144);
+resutado = Math.abs(-200);
+resutado = Math.max( 3, 5, 1, 8, 2, 10);
+resutado = Math.min( 3, 5, 1, 8, 2, 10);
+resutado = Math.random();
+resutado = Math.floor( Math.random()*3);
+
+console.log(resutado);
