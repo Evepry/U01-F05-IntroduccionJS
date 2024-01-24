2d2db62a9bc436153d7cc9c33e955ae6f8d29887
Priscy Carrillo <priscy.carrillo42@gmail.com>
2024-01-23 18:09:28 -0600

    C104M20240123

---
+++ js/02.js
@@ -0,0 +1,34 @@
+// Variables con let
+
+let producto = 'Audifonos Gamer'; // Iniciar una variable y asignarle valor
+
+let disponible; // Iniciamos una variable pero sin valor
+alert (producto);
+
+producto = true; // Reasignando el valor de la variable
+
+alert (producto); 
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+    alert (producto1 + disponible1 + categoria);
+
+// Variables no permitidas
+/*
+let 1disponible;
+let nombre-producto;
+*/
+
+// Variables permitidas
+let disponible_;
+
+// Estilos para las variables
+let nombre_producto = 'monitor HD'; // underscore
+let nombreProducto = 'monitorHD';   // Camelcase
+let NombreProducto = 'Monitor HD';  // Pascal Case
+let nombreproducto = 'monitorhd';
+
+console.log(producto);
+console.log('Hola Mundo');
---
+++ js/03.js
@@ -0,0 +1,37 @@
+// Variables con const
+
+const producto = 'Audifonos Gamer'; // Iniciar una variable y asignarle valor
+
+// const disponible;
+// Con const no se peude iniciar una variable sin valor
+
+
+alert (producto);
+
+// producto = true;
+// No se puede reasignando el valor de la variable
+
+alert (producto); 
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+    alert (producto1 + disponible1 + categoria);
+
+// Variables no permitidas
+/*
+const 1disponible;
+const nombre-producto;
+*/
+
+const disponible_1 = 0;
+
+// Estilos para las variables
+const nombre_producto = 'monitor HD'; // underscore
+const nombreProducto = 'monitorHD';   // Camelcase
+const NombreProducto = 'Monitor HD';  // Pascal Case
+const nombreproducto = 'monitorhd';
+
+console.log(producto);
+console.log('Hola Mundo');
