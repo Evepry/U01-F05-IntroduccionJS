4180fd5924fb122131bd5d39f6cee44a2c65742d
Priscy Carrillo <priscy.carrillo42@gmail.com>
2024-01-26 17:05:27 -0600

    C111M20240126

---
+++ js/08.js
@@ -0,0 +1,14 @@
+const nombre = 'Priscy';
+const email = 'correo@correo.com';
+
+// CONCATENACION
+
+console.log(nombre + email);
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template String - Sring Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
+// No funciona cualquier comilla
+console.log('Nombre Cliente: ${nombre} Email: ${email}');
+
