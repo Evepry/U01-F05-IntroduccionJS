37b63857fc740cd5f5b9ded1f760f62c7113542e
Priscy Carrillo <priscy.carrillo42@gmail.com>
2024-01-25 20:47:45 -0600

    C110M20240125

--- js/07.js
+++ js/07.js
@@ -4,4 +4,20 @@ let resultado;
 
 resultado = 20 + 30 * 2;
 
+resultado = (600 + 600) * 1.05;
+
 console.log(resultado);
+
+// Incrementos
+
+let puntaje1 = 10;
+
+// Incremento en 1++
+
+puntaje1 += 2;
+
+console.log(puntaje1);
+
+puntaje1 += 2;
+
+console.log(puntaje1);
